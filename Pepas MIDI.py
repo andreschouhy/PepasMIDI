c3bbd0606582f1ac9aaefda640f2c40af25e7b99
andreschouhy <andreschouhy@gmail.com>
2023-07-19 19:57:59 -0300

    fix

--- Pepas MIDI.py
+++ Pepas MIDI.py
@@ -61,18 +61,20 @@ presetActual = 1
 midiout = rtmidi.MidiOut()
 midiin = rtmidi.MidiIn()
 
-available_ports_out = midiout.get_ports()
-available_ports_in = midiin.get_ports()
+# available_ports_out = midiout.get_ports()
+#available_ports_in = midiin.get_ports()
 
-if available_ports_out:
-	midiout.open_port(0)
-else:
-	midiout.open_virtual_port("Pepas MIDI Out")
+# if available_ports_out:
+# 	midiout.open_port(0)
+# else:
+# 	midiout.open_virtual_port("Pepas MIDI Out")
+midiout.open_virtual_port("Pepas MIDI Out")
 
-if available_ports_in:
-	midiin.open_port(0)
-else:
-	midiin.open_virtual_port("Pepas MIDI In")
+# if available_ports_in:
+# 	midiin.open_port(0)
+# else:
+# 	midiin.open_virtual_port("Pepas MIDI In")
+midiin.open_virtual_port("Pepas MIDI In")
 
 def mapKeyToMIDI(k):
     dic = {
